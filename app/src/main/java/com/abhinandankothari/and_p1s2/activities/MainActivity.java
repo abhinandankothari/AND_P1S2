bf24d7b0fed29ac575d7aaef7fb2c87a2ded668a
Abhinandan Kothari <abhinandankothari@gmail.com>
2016-04-14 06:14:16 +0530

    Created MainActivityFragment

--- app/src/main/java/com/abhinandankothari/and_p1s2/activities/MainActivity.java
+++ app/src/main/java/com/abhinandankothari/and_p1s2/activities/MainActivity.java
@@ -1,66 +1,28 @@
 package com.abhinandankothari.and_p1s2.activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.abhinandankothari.and_p1s2.R;
-import com.abhinandankothari.and_p1s2.adapters.RecyclerViewAdapter;
-import com.abhinandankothari.and_p1s2.contract.Movie;
-import com.abhinandankothari.and_p1s2.listeners.MoviesViewTouchListener;
-import com.abhinandankothari.and_p1s2.listeners.OnMoviesTouchListener;
-import com.abhinandankothari.and_p1s2.network.Api;
-import com.abhinandankothari.and_p1s2.utility.ConnectionDetector;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    private GridLayoutManager gridLayoutManager;
-    private RecyclerViewAdapter recyclerViewAdapter;
 
-    @Bind(R.id.recycler_view)
-    RecyclerView moviesView;
-    @Bind(R.id.toolbar)
-    Toolbar toolbar;
+//    @Bind(R.id.toolbar)
+//    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
-        gridLayoutManager = new GridLayoutManager(this, 2);
-        moviesView.setHasFixedSize(true);
-        moviesView.setLayoutManager(gridLayoutManager);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        addTouchListenerToMoviesList();
-    }
-
-    private void addTouchListenerToMoviesList() {
-        moviesView.addOnItemTouchListener(new MoviesViewTouchListener(this, new OnMoviesTouchListener() {
-            @Override
-            public void onItemClick(View childView, int position) {
-                Intent movieIntent = new Intent(MainActivity.this, DetailActivity.class);
-                movieIntent.putExtra(Movie.TAG, recyclerViewAdapter.get(position));
-                startActivity(movieIntent);
-            }
-        }));
     }
 
     @Override
@@ -72,14 +34,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        FetchMoviesTask task = new FetchMoviesTask();
-        ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
-        Boolean isInternetPresent = cd.isConnectingToInternet();
-        if (isInternetPresent) {
-            task.execute();
-        } else {
-            Toast.makeText(this, "You are offline, Check your Internet Connection or Switch to Favourite Movies", Toast.LENGTH_LONG);
-        }
     }
 
     @Override
@@ -92,49 +46,5 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private List<Movie> fetchMoviesList() {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        String sort = sharedPref.getString("sort", "Popularity");
-        List<Movie> allItems = new ArrayList<Movie>();
-        if (!sort.equalsIgnoreCase("My Favourites")) {
-            Api movies = new Api();
-            for (Movie movie : movies.ListofMovies(sort)) {
-                allItems.add(new Movie(movie.getId(), movie.getMovieTitle(), movie.getMoviePosterThumbUrl(), movie.getMovieSynopsis(),
-                        movie.getMovieRating(), movie.getMovieReleaseDate()));
-            }
-            return allItems;
-        } else {
-            //Content Provider
-            Cursor cursor = getContentResolver().query(Movie.CONTENT_URI, null, null, null, null);
-            int i;
-            for (i = 0; i < cursor.getCount(); i++) {
-                cursor.moveToPosition(i);
-                allItems.add(i, new Movie(
-                        cursor.getInt(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_ID)),
-                        cursor.getString(cursor.getColumnIndex(Movie.COLUMN_MOVIE_TITLE)),
-                        cursor.getString(cursor.getColumnIndex(Movie.COLUMN_MOVIE_POSTER_PATH)),
-                        cursor.getString(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_SYNOPSIS)),
-                        cursor.getString(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_RATING)),
-                        cursor.getString(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_RELEASE_DATE))));
-            }
-            cursor.close();
-            return allItems;
-        }
-    }
-
-    public class FetchMoviesTask extends AsyncTask<Void, Void, List<Movie>> {
-        @Override
-        protected void onPostExecute(List<Movie> movieList) {
-            super.onPostExecute(movieList);
-            recyclerViewAdapter = new RecyclerViewAdapter(getApplicationContext(), movieList);
-            moviesView.setAdapter(recyclerViewAdapter);
-        }
-
-        @Override
-        protected List<Movie> doInBackground(Void... params) {
-            return fetchMoviesList();
-        }
     }
 }
---
+++ app/src/main/java/com/abhinandankothari/and_p1s2/fragments/MainActivityFragment.java
@@ -0,0 +1,128 @@
+package com.abhinandankothari.and_p1s2.fragments;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.abhinandankothari.and_p1s2.R;
+import com.abhinandankothari.and_p1s2.activities.DetailActivity;
+import com.abhinandankothari.and_p1s2.adapters.RecyclerViewAdapter;
+import com.abhinandankothari.and_p1s2.contract.Movie;
+import com.abhinandankothari.and_p1s2.listeners.MoviesViewTouchListener;
+import com.abhinandankothari.and_p1s2.listeners.OnMoviesTouchListener;
+import com.abhinandankothari.and_p1s2.network.Api;
+import com.abhinandankothari.and_p1s2.utility.ConnectionDetector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivityFragment extends Fragment {
+    private GridLayoutManager gridLayoutManager;
+    private RecyclerViewAdapter recyclerViewAdapter;
+
+    @Bind(R.id.recycler_view)
+    RecyclerView moviesView;
+
+    public MainActivityFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, rootView);
+        gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        moviesView.setHasFixedSize(true);
+        moviesView.setLayoutManager(gridLayoutManager);
+        addTouchListenerToMoviesList();
+        return rootView;
+    }
+
+    private void addTouchListenerToMoviesList() {
+        moviesView.addOnItemTouchListener(new MoviesViewTouchListener(getActivity(), new OnMoviesTouchListener() {
+            @Override
+            public void onItemClick(View childView, int position) {
+                Intent movieIntent = new Intent(getActivity(), DetailActivity.class);
+                movieIntent.putExtra(Movie.TAG, recyclerViewAdapter.get(position));
+                startActivity(movieIntent);
+            }
+        }));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        FetchMoviesTask task = new FetchMoviesTask();
+        ConnectionDetector cd = new ConnectionDetector(getActivity());
+        Boolean isInternetPresent = cd.isConnectingToInternet();
+        if (isInternetPresent) {
+            task.execute();
+        } else {
+            Toast.makeText(getActivity(), "You are offline, Check your Internet Connection or Switch to Favourite Movies", Toast.LENGTH_LONG);
+        }
+    }
+
+    public class FetchMoviesTask extends AsyncTask<Void, Void, List<Movie>> {
+        @Override
+        protected void onPostExecute(List<Movie> movieList) {
+            super.onPostExecute(movieList);
+            recyclerViewAdapter = new RecyclerViewAdapter(getActivity(), movieList);
+            moviesView.setAdapter(recyclerViewAdapter);
+        }
+
+        @Override
+        protected List<Movie> doInBackground(Void... params) {
+            return fetchMoviesList();
+        }
+    }
+
+    private List<Movie> fetchMoviesList() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sort = sharedPref.getString("sort", "Popularity");
+        List<Movie> allItems = new ArrayList<Movie>();
+        if (!sort.equalsIgnoreCase("My Favourites")) {
+            Api movies = new Api();
+            for (Movie movie : movies.ListofMovies(sort)) {
+                allItems.add(new Movie(movie.getId(), movie.getMovieTitle(), movie.getMoviePosterThumbUrl(), movie.getMovieSynopsis(),
+                        movie.getMovieRating(), movie.getMovieReleaseDate()));
+            }
+            return allItems;
+        } else {
+            //Content Provider
+            Cursor cursor = getActivity().getContentResolver().query(Movie.CONTENT_URI, null, null, null, null);
+            int i;
+            for (i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToPosition(i);
+                allItems.add(i, new Movie(
+                        cursor.getInt(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_ID)),
+                        cursor.getString(cursor.getColumnIndex(Movie.COLUMN_MOVIE_TITLE)),
+                        cursor.getString(cursor.getColumnIndex(Movie.COLUMN_MOVIE_POSTER_PATH)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_SYNOPSIS)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_RATING)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(Movie.COLUMN_MOVIE_RELEASE_DATE))));
+            }
+            cursor.close();
+            return allItems;
+        }
+    }
+}
